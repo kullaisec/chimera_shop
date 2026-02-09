b46438e6e377e97a8bc44f62dd59ef102a8b3436
Kullai Metikala <99985908+kullaisec@users.noreply.github.com>
2026-02-09 22:19:51 +0530

    Create auth.py

---
+++ routes/auth.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, request
+from services.auth_service import login
+
+auth_bp = Blueprint("auth", __name__)
+
+@auth_bp.route("/login", methods=["POST"])
+def login_route():
+    return login(request.json.get("user_id"))
